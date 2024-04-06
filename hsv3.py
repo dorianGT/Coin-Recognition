6ff07af639fb4b2daa4e0f8cd7464f3d32286e4c
olivier jiang <jiangolivier@yahoo.fr>
2024-04-07 00:33:55 +0200

    update

--- hsv3.py
+++ hsv3.py
@@ -2,8 +2,7 @@ import cv2
 import numpy as np
 import os
 
-folder_path = "Dataset"  # Replace with the path to your folder
-
+folder_path = "Dataset2"
 for filename in os.listdir(folder_path):
     if filename.endswith((".jpg", ".jpeg", ".png", ".HEIC")):
         image_path = os.path.join(folder_path, filename)
@@ -13,26 +12,67 @@ for filename in os.listdir(folder_path):
         hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
         # Seuillage pour la couleur jaune
-        lower_yellow = np.array([20, 120, 120])
-        upper_yellow = np.array([30, 255, 255])
+        lower_yellow = np.array([18, 90, 90])
+        upper_yellow = np.array([25, 210, 230])
         mask_yellow = cv2.inRange(hsv_image, lower_yellow, upper_yellow)
 
         # Seuillage pour la couleur rouge
-        lower_red = np.array([0, 50, 50])
-        upper_red = np.array([10, 255, 255])
+        lower_red = np.array([3, 100, 60])
+        upper_red = np.array([10, 210, 230])
         mask_red = cv2.inRange(hsv_image, lower_red, upper_red)
 
-        # Combinaison des masques jaune et rouge
-        masque_final = mask_yellow | mask_red
+        # Seuillage pour la couleur grise (adapter les plages selon les besoins)
+        lower_gray = np.array([3, 10, 100])
+        upper_gray = np.array([140, 55, 220])
+        mask_gray = cv2.inRange(hsv_image, lower_gray, upper_gray)
 
-        # Application du masque à l'image originale
-        image_segmentée = cv2.bitwise_and(image, image, mask=masque_final)
+        image_segmentee_rouge = cv2.bitwise_and(image, image, mask=mask_red)
+        image_segmentee_jaune = cv2.bitwise_and(image, image, mask=mask_yellow)
+        image_segmentee_gris = cv2.bitwise_and(image, image, mask=mask_gray)
 
-        image = cv2.resize(image, (0, 0), fx=0.3, fy=0.3)
-        image_segmentée = cv2.resize(image_segmentée, (0, 0), fx=0.3, fy=0.3)
+        # Compter le nombre de pixels dans les régions de couleur rouge, jaune et grise
+        red_pixel_count = cv2.countNonZero(mask_red)
+        yellow_pixel_count = cv2.countNonZero(mask_yellow)
+        gray_pixel_count = cv2.countNonZero(mask_gray) 
 
-        # Affichage de l'image segmentée
-        cv2.imshow(image_path, image)
-        cv2.imshow(image_path+" segmenté", image_segmentée)
+        if gray_pixel_count > 0.5 * image[:,:,0].size:
+            print("1 euro")
+        elif 0.15 * image[:,:,0].size < gray_pixel_count < 0.4 * image[:,:,0].size:
+            print("2 euros")
+        elif yellow_pixel_count > 0.5 * image[:,:,0].size:
+            print(" piece jaune 1e intervalle")
+        elif red_pixel_count > 0.5 * image[:,:,0].size:
+            print("piece rouge 1e intervalle")
+        else:
+            lower_yellow = np.array([11, 120, 90])
+            upper_yellow = np.array([17, 210, 255])
+            mask_yellow = cv2.inRange(hsv_image, lower_yellow, upper_yellow)
+
+            lower_red = np.array([10, 100, 60])
+            upper_red = np.array([17, 210, 230])
+            mask_red = cv2.inRange(hsv_image, lower_red, upper_red)
+
+            image_segmentee_rouge = cv2.bitwise_and(image, image, mask=mask_red)
+            image_segmentee_jaune = cv2.bitwise_and(image, image, mask=mask_yellow)
+
+            red_pixel_count = cv2.countNonZero(mask_red)
+            yellow_pixel_count = cv2.countNonZero(mask_yellow)
+        
+            if red_pixel_count > 0.5 * image[:,:,0].size:
+                print(" piece rouge 2e intervalle")
+            elif yellow_pixel_count > 0.5 * image[:,:,0].size:
+                print("piece jaune 2e intervalle")
+            
+            else:
+                print(" jsp")
+        image = cv2.resize(image, (500, 500))
+        image_segmentee_rouge = cv2.resize(image_segmentee_rouge,  (500, 500))
+        image_segmentee_jaune = cv2.resize(image_segmentee_jaune, (500, 500))
+        image_segmentee_gris = cv2.resize(image_segmentee_gris, (500, 500))
+        cv2.imshow("Image", image)
+        cv2.imshow("rouge", image_segmentee_rouge)
+        cv2.imshow("jaune", image_segmentee_jaune)
+        cv2.imshow("gris", image_segmentee_gris)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
+            
